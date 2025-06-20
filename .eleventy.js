843d565a50dd8b4b03dd25cc1e788faa1115704c
Allie Lapinski <alex.lapinski@gmail.com>
2025-06-20 17:43:31 -0400

    Baseline complete

--- .eleventy.js
+++ .eleventy.js
@@ -1,37 +1,75 @@
+const { DateTime } = require("luxon");
+const path = require('path');
+const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
+const eleventyRssPlugin = require("@11ty/eleventy-plugin-rss");
+const eleventySitemapPlugin = require("@quasibit/eleventy-plugin-sitemap");
+const markdownIt = require("markdown-it");
+const markdownItAnchor = require("markdown-it-anchor");
+
 module.exports = function(eleventyConfig) {
-  // Add plugins
-  eleventyConfig.addPlugin(require("@11ty/eleventy-navigation"));
-  eleventyConfig.addPlugin(require("@11ty/eleventy-plugin-rss"));
-  eleventyConfig.addPlugin(require("@quasibit/eleventy-plugin-sitemap"));
-  
-  // Add markdown-it plugins for better markdown support
-  let markdownIt = require("markdown-it");
-  let markdownItAnchor = require("markdown-it-anchor");
-  
-  let options = {
+  // Plugins
+  eleventyConfig.addPlugin(eleventyNavigationPlugin);
+  eleventyConfig.addPlugin(eleventyRssPlugin);
+  eleventyConfig.addPlugin(eleventySitemapPlugin, {
+    sitemap: {
+      hostname: "https://trans-history.netlify.app", // Replace with your domain
+    },
+  });
+
+  // Markdown-it configuration
+  const markdownLib = markdownIt({
     html: true,
     breaks: true,
-    linkify: true
-  };
-  
-  let markdownLib = markdownIt(options)
-    .use(markdownItAnchor, {
-      permalink: markdownItAnchor.permalink.ariaHidden({
-        placement: "after",
-        class: "direct-link",
-        symbol: "#",
-        level: [1,2,3]
-      })
-    });
-  
+    linkify: true,
+  }).use(markdownItAnchor, {
+    permalink: markdownItAnchor.permalink.ariaHidden({
+      placement: "after",
+      class: "direct-link",
+      symbol: "#",
+      level: [1, 2, 3],
+    }),
+  });
   eleventyConfig.setLibrary("md", markdownLib);
-  
+
+  // Date and time filters
+  eleventyConfig.addFilter("readableDate", (dateObj) => {
+    return DateTime.fromJSDate(dateObj, { zone: 'utc' }).toFormat('LLLL d, yyyy');
+  });
+  eleventyConfig.addFilter('w3cDate', (dateObj) => {
+    return DateTime.fromJSDate(dateObj, { zone: 'utc' }).toISO();
+  });
+
+  // Shortcode for current year
+  eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
+
+  // Collections
+  eleventyConfig.addCollection("figuresByYear", (collectionApi) => {
+    return collectionApi.getFilteredByGlob("./src/_data/*.md").sort((a, b) => {
+      const yearA = a.fileSlug.split('-')[0];
+      const yearB = b.fileSlug.split('-')[0];
+      return yearA - yearB;
+    });
+  });
+
+  // Passthrough copy
+  eleventyConfig.addPassthroughCopy("src/css");
+  eleventyConfig.addPassthroughCopy("src/js");
+  eleventyConfig.addPassthroughCopy("src/images");
+  eleventyConfig.addPassthroughCopy("src/favicon.ico");
+
+  // Directory configuration
   return {
     dir: {
       input: "src",
       includes: "_includes",
+      layouts: "_includes/layouts", // Correctly define layouts directory
       data: "_data",
-      output: "_site"
-    }
+      output: "_site",
+    },
+    templateFormats: ["njk", "md", "html"],
+    markdownTemplateEngine: "njk",
+    htmlTemplateEngine: "njk",
+    dataTemplateEngine: "njk",
+    passthroughFileCopy: true,
   };
-}
+};
